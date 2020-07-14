9aa047a2f9063e48dc40562838b747451b90fac0
PresentKim <debe3721@gmail.com>
2020-07-14 16:25:13 +0900

    /sk : 파티클 테스트 명령어 추가

--- src/kim/present/testcommands/TestCommands.php
+++ src/kim/present/testcommands/TestCommands.php
@@ -30,6 +30,7 @@ use pocketmine\block\BlockBreakInfo;
 use pocketmine\block\BlockIdentifier;
 use pocketmine\block\BlockLegacyIds;
 use pocketmine\block\BlockToolType;
+use pocketmine\color\Color;
 use pocketmine\command\Command;
 use pocketmine\command\CommandExecutor;
 use pocketmine\command\CommandSender;
@@ -42,6 +43,7 @@ use pocketmine\nbt\JsonNbtParser;
 use pocketmine\nbt\NbtDataException;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use pocketmine\world\particle\DustParticle;
 
 class TestCommands extends PluginBase implements Listener{
     /**
@@ -127,6 +129,42 @@ class TestCommands extends PluginBase implements Listener{
                 for($y = 0, $height = mt_rand(3, 6); $y < $height; ++$y){
                     $sender->getWorld()->setBlock($sender->getPosition()->floor()->add(0, $y, 0), new Block(new BlockIdentifier(BlockLegacyIds::LOG, 0), "t", new BlockBreakInfo(0.1, BlockToolType::NONE)));
                 }
+                return true;
+            }
+        }));
+        $this->getServer()->getCommandMap()->register(strtolower($this->getName()), new PluginCommand('sk', $this, new class() implements CommandExecutor{
+            public function onCommand(CommandSender $sender, Command $command, $label, array $params) : bool{
+                if(!$sender instanceof Player){
+                    $sender->sendMessage(TextFormat::YELLOW . "인게임에서 실행하세요");
+                    return true;
+                }
+
+                $vec = $sender->getPosition()->addVector($sender->getDirectionVector()->multiply(5));
+                $world = $sender->getWorld();
+                $particle = new DustParticle(new Color(255, 35, 0));
+                for($i = -2; $i <= 2; $i += 0.1){
+                    $world->addParticle($vec->add($i, 1, -1), $particle);
+                }
+
+                for($i = 0; $i <= 1; $i += 0.1){
+                    $x = $i / 0.5;
+                    $y = $i;
+                    $world->addParticle($vec->add($x, $y, -1), $particle);
+                    $world->addParticle($vec->add(-$x, $y, -1), $particle);
+                }
+
+                for($i = -1; $i <= 0; $i += 0.1){
+                    $x = $i / 1;
+                    $world->addParticle($vec->add($x, $i, -1), $particle);
+                    $world->addParticle($vec->add(-$x, $i, -1), $particle);
+                }
+
+                for($i = -2; $i <= 2; $i += 0.1){
+                    $x = $i / 4;
+                    $world->addParticle($vec->add($x - 0.5, $i + 1, -1), $particle);
+                    $world->addParticle($vec->add(-$x + 0.5, $i + 1, -1), $particle);
+                }
+
                 return true;
             }
         }));
